34731c4440880682a827d78e25bc1561e3ad5345
Hoang Duc <huyduk19@gmail.com>
2020-10-12 14:27:02 +0700

    day du an lowen len

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// Tạo menu cố định khi cuộn : start
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("menu");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("fixed-top")
+	} else {
+		navbar.classList.remove("fixed-top");
+	}
+}
+// Tạo menu cố định khi cuộn : end
+
+
+// owl-carousel-category : start
+$('.owl-category').owlCarousel({
+	loop:true,
+	margin:10,
+	responsiveClass:true,
+	responsive:{
+		0:{
+			items:2,
+			nav:false
+		},
+		600:{
+			items:3,
+			nav:false
+		},
+		1000:{
+			items:5,
+			nav:true,
+			loop:false
+		}
+	}
+})
+// owl-carousel-category : end
+
+// owl-carousel-bathroom : start
+$('.owl-bathroom').owlCarousel({
+	autoplay: true,
+	center: true,
+	loop: true,
+	nav: true,
+})
+// owl-carousel-bathroom : end
